55921f4cf5565581bc7d5b4681f8fb06b19e19ac
Sriyansh Srivastava <sriyanshsrivastava.dev@gmail.com>
2026-03-25 02:21:36 +0530

    cloude-chef: feat: list all input ingredients below the form

--- cloude-chef/src/components/Form.jsx
+++ cloude-chef/src/components/Form.jsx
@@ -1,14 +1,49 @@
+import {useState} from "react";
+
 export default function Form() {
-  return (
+
+    const [ingredients,setIngredients] = useState([]);
+
+
+    const ingredientItems = ingredients.map((ingredient,index)=>(
+        <li className={"font-semibold py-1 list-disc"} key={index}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        if (!newIngredient) return;
+
+
+        setIngredients(prev => {
+            const normalized = newIngredient.trim().toLowerCase();
+
+            if (prev.map(i => i.toLowerCase()).includes(normalized)) {
+                return prev;
+            }
+
+            return [
+                ...prev,
+                normalized.charAt(0).toUpperCase() + normalized.slice(1),
+            ];
+        });
+        event.currentTarget.reset();
+
+    }
+
+    return (
     <main>
       <div className="flex justify-center items-center mt-10 pt-16">
-        <form className="flex space-x-2 w-full max-w-lg">
+        <form className="flex space-x-2 w-full max-w-lg" onSubmit={handleSubmit}>
           <input
-            className="flex-1 px-5 py-2 border-2 border-amber-200  hover:border-slate-950 shadow-lg rounded-full focus:bg-slate-50"
+            className="flex-1 px-5 py-2 border-2 border-amber-100/80  hover:border-slate-950/10 shadow-lg rounded-full focus:bg-slate-50"
             type="text"
             placeholder="e.g. oregano"
             aria-label="Add indredient"
+            name = "ingredient"
           />
+
           <button
             className={
               "bg-slate-900 hover:bg-slate-950 hover:shadow-lg text-white font-semibold px-4 py-2 rounded-full cursor-pointer before:content-['+'] before:mr-2 text-sm"
@@ -19,6 +54,12 @@ export default function Form() {
           </button>
         </form>
       </div>
+        <div className="flex flex-col justify-start items-start mt-10 px-20 w-screen ">
+            <h1 className={"text-2xl font-bold "}>Ingredients List</h1>
+            <ul className="mt-5 px-10">
+                {ingredientItems}
+            </ul>
+        </div>
     </main>
-  );
+  )
 }
